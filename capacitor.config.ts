a78497ce6805f1fb75b9d26cfbbe9b9cfd3e8ea2
SeuNomeGitHub <seuemail@exemplo.com>
2026-08-31 22:17:52 -0300

    fix: desativar splash screen e adicionar fallback visual no HTML

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,15 @@ const config: CapacitorConfig = {
   appId: 'com.fonix.app',
   appName: 'fonix',
   webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+      launchAutoHide: true,
+      backgroundColor: "#ffffff",
+      androidSplashResourceName: "splash",
+      showSpinner: false
+    }
+  },
   server: {
     androidScheme: 'https',
     cleartext: true
