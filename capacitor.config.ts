806e3141135164098d73dfbdc57f15e2e5c8124e
SeuNomeGitHub <seuemail@exemplo.com>
2026-08-31 22:03:53 -0300

    fix: build web corrigido com alias e base path relativos

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,12 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.fonix.app',
-  appName: 'Fônix',
-  webDir: 'dist'
+  appName: 'fonix',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
 };
 
 export default config;
-
